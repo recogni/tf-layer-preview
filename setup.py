ec3a67e18976c98eab3450f5130a91d34e273ce1
Shaba Abhiram <shabarivas.abhiram@gmail.com>
2018-07-29 00:19:20 -0700

    Rename python lib dir

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name="tf_layer_preview",
-    version="0.0.1",
+    version="0.0.2",
     description="Tensorflow layer previewer",
     url="https://github.com/recogni/tf-layer-preview",
     author="sabhiram",
     install_requires=[
         "tornado"
     ],
-    packages=find_packages())
+    packages=["tf_layer_preview"],
+    package_dir={"tf_layer_preview": "preview"})
